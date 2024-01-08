cadd1707274d7b1186fa5beaca0835dc6054aea4
AlessiaOsman <alessia.osman@gmail.com>
2024-01-08 16:28:03 +0100

    added while

--- js/script.js
+++ js/script.js
@@ -5,8 +5,16 @@ const myList = ['Pane', 'Pasta', 'Vino', 'Mele', 'Noci', 'Insalata', 'Gelato'];
 
 let listItems = '<ul class="list">';
 
-for (let i = 0; i<myList.length; i++){
+/*for (let i = 0; i<myList.length; i++){
     listItems += `<li>${myList[i]}</li>`
+}*/
+
+let i = 0
+
+while (i < myList.length){
+
+    listItems += `<li>${myList[i]}</li>`
+    i++
 }
 
 listItems += '</ul>';
